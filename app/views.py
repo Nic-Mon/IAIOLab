493a79fec177e888e23eda4e9afb57012070b3e2
Nic-Mon <n_mon@berkeley.edu>
2018-04-24 13:45:01 -0700

    include np

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, request, session
 from app import app, models
 import random 
+import numpy as np
 
 georgeblood_ids = np.load('georgeblood_ids.npy').tolist()
 
